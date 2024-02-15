87aae1e7626fae74ad3416795a40bf6d4689278b
briandproctor <160077724+briandproctor@users.noreply.github.com>
2024-02-15 17:18:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# does some f'dup stuff
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# i'd prefer this do nothing?
+streamlit.dataframe(fruityvice_normalized)
